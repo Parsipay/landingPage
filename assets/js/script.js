b93501fb553f1d7304f1b3e25d82cea3619398d3
banafshe <banafsheebrahimi931@gmail.com>
2025-05-28 16:33:01 +0330

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,50 @@
- $('.fa-solid').on('click', function () {
-    $(this).addClass('d-none'); 
-    $(this).siblings('.fa-minus').removeClass('d-none');    
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,            
+    },
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,            
+    },
+  },
+});
+
+  $(function() {
+    const now = new Date();
+    const formatter = new Intl.DateTimeFormat('fa-IR', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    });
+
+    const parts = formatter.formatToParts(now);
+    let day = '', month = '', year = '';
+
+    parts.forEach(part => {
+      if (part.type === 'day') day = part.value;
+      if (part.type === 'month') month = part.value;
+      if (part.type === 'year') year = part.value;
+    });
+
+    const formattedDate = `${day} ${month} ${year}`;
+    $('.today-date').text(formattedDate);
   });
-
-  $('.fa-minus').on('click', function () {
-    $(this).addClass('d-none');   
-    $(this).siblings('.fa-solid').removeClass('d-none');    
-  });
-
-
